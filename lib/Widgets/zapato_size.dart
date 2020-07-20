f91bcb033d063e8a7c7f2ac002aac78f914aff19
Alejandro <alejandro.mancilla.umsa@gmail.com>
2020-07-19 23:12:22 -0400

    sombra

--- lib/Widgets/zapato_size.dart
+++ lib/Widgets/zapato_size.dart
@@ -30,10 +30,42 @@ class _ZapatoConSombra extends StatelessWidget {
       padding: EdgeInsets.all(50.0),
       child: Stack(
         children: [
-          Image(image: AssetImage("assets/imgs/azul.png")),
+          Positioned(
+            bottom: 20.0,
+            right: 0.0,
+            child: _sombra()
+          ),
+          Image(image: AssetImage("assets/imgs/azul.png"),),
           
         ],
         
+      ),
+    );
+  }
+}
+
+class _sombra extends StatelessWidget {
+  const _sombra({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Transform.rotate(
+      angle: -0.5,
+      child: Container(
+        //color: Colors.pink,
+        width: 230.0,
+        height: 100.0,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(100.0),
+          boxShadow: [
+            BoxShadow(
+              color: Color(0xffEAA14E),
+              blurRadius: 20.0
+            )
+          ]
+        ),
       ),
     );
   }
