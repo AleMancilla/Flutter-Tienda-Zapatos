9bee708d1a0d080d39e171fd6a1fae32ad4c1c30
Alejandro <alejandro.mancilla.umsa@gmail.com>
2020-07-19 23:31:15 -0400

    tallas

--- lib/Widgets/zapato_size.dart
+++ lib/Widgets/zapato_size.dart
@@ -15,7 +15,8 @@ class ZapatoSize extends StatelessWidget {
 
         child: Column(
           children: [
-            _ZapatoConSombra()
+            _ZapatoConSombra(),
+            _ZapatoTallas()
           ],
         ),
       ),
@@ -66,6 +67,57 @@ class _sombra extends StatelessWidget {
             )
           ]
         ),
+      ),
+    );
+  }
+}
+
+
+class _ZapatoTallas extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          _TallaZapatoCaja(7),
+          _TallaZapatoCaja(7.5),
+          _TallaZapatoCaja(8),
+          _TallaZapatoCaja(8.5),
+          _TallaZapatoCaja(9),
+          _TallaZapatoCaja(9.5),
+        ],
+      ),
+    );
+  }
+}
+
+class _TallaZapatoCaja extends StatelessWidget {
+  
+  final double numero;
+  _TallaZapatoCaja(this.numero);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 45.0,
+      height: 45.0,
+      // color: Colors.red,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          // BoxShadow()
+        ]
+      ),
+      child: Text("${numero.toString().replaceAll(".0", "")}",style: TextStyle(
+        color: Color(0xffF1A23A),
+        fontSize: 16.0,
+        fontWeight: FontWeight.bold
+      ),
+      
       ),
     );
   }
