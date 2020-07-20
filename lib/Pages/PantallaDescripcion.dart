590ae75f1526433ceab1ade4c57119db9f8591d4
Alejandro <alejandro.mancilla.umsa@gmail.com>
2020-07-20 19:27:21 -0400

    boton de compra

--- lib/Pages/PantallaDescripcion.dart
+++ lib/Pages/PantallaDescripcion.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shoesapp/Widgets/BotonNaranja.dart';
 import 'package:shoesapp/Widgets/ZapatoDesc.dart';
 import 'package:shoesapp/Widgets/zapato_size.dart';
 
@@ -18,14 +19,32 @@ class PantallaDescripsion extends StatelessWidget {
                         titulo: 'Nike Air Max 720',
                         descripcion: "The Nike Air Max 720 goes bigger than ever before with Nike's taller Air unit yet, offering more air underfoot for unimaginable, all-day comfort. Has Air Max gone too far? We hope so.",
                       ),
-                      ZapatosDescripcion(
-                        titulo: 'Nike Air Max 720',
-                        descripcion: "The Nike Air Max 720 goes bigger than ever before with Nike's taller Air unit yet, offering more air underfoot for unimaginable, all-day comfort. Has Air Max gone too far? We hope so.",
-                      ),
+                      _MontoBuy()
                   ] 
                 ),
               ),
             ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _MontoBuy extends StatelessWidget {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30.0),
+      child: Container(
+        margin: EdgeInsets.only(top: 20.0),
+        child: Row(
+          children: [
+            Text("\$180",style: TextStyle(fontSize: 28.0, fontWeight: FontWeight.bold),),
+            Spacer(),
+            BotonNaranja(texto: "Buy Now",alto: 40.0,ancho: 120.0,)
           ],
         ),
       ),
--- lib/Widgets/BotonNaranja.dart
+++ lib/Widgets/BotonNaranja.dart
@@ -2,13 +2,15 @@ import 'package:flutter/material.dart';
 
 class BotonNaranja extends StatelessWidget {
   final String texto;
-  BotonNaranja({@required this.texto});
+  final double alto;
+  final double ancho;
+  BotonNaranja({@required this.texto, this.alto = 50.0, this.ancho=150.0});
   @override
   Widget build(BuildContext context) {
     return Container(
       alignment: Alignment.center,
-      width: 150.0,
-      height: 50.0,
+      width: this.ancho,
+      height: this.alto,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(100.0),
         color: Colors.orange
